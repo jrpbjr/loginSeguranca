701060e189dc7ccc8bafdc051a8fe637a4025e7a
jrpbjr <jrpbjr@gmail.com>
2024-05-18 14:29:51 -0300

    Implementando o servico de login , autenticaçao e regra do usuario.

---
+++ src/main/java/org/exemple/service/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,57 @@
+package org.exemple.service.impl;
+
+import org.exemple.dao.request.SignUpRequest;
+import org.exemple.dao.request.SigninRequest;
+import org.exemple.dao.response.JwtAuthenticationResponse;
+import org.exemple.entities.Role;
+import org.exemple.entities.User;
+import org.exemple.repository.UserRepository;
+import org.exemple.service.AuthenticationService;
+import org.exemple.service.JwtService;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public JwtAuthenticationResponse signup(SignUpRequest request) {
+        var user = User.builder().firstName(request.getFirstName()).lastName(request.getLastName())
+                .email(request.getEmail()).password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER).build();
+        userRepository.save(user);
+        var jwt = jwtService.generateToken(user);
+        return JwtAuthenticationResponse.builder().token(jwt).build();
+    }
+
+    @Override
+    public JwtAuthenticationResponse signin(SigninRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+        var jwt = jwtService.generateToken(user);
+        return JwtAuthenticationResponse.builder().token(jwt).build();
+    }
+
+
+}
