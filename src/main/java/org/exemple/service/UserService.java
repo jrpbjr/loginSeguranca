b191bfe48839fbf510192191618613c6127b7d50
jrpbjr <jrpbjr@gmail.com>
2024-05-19 10:47:49 -0300

    adicionando anotação como componet

--- src/main/java/org/exemple/service/UserService.java
+++ src/main/java/org/exemple/service/UserService.java
@@ -1,8 +1,9 @@
 package org.exemple.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public interface UserService {
     UserDetailsService userDetailsService();
 }
