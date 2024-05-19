8d2cc8c44ad638a39c85a4bdd1c9864ff21e05bb
jrpbjr <jrpbjr@gmail.com>
2024-05-19 10:46:52 -0300

    controller url  http://localhost:8090/api/v1/resource

---
+++ src/main/java/org/exemple/controller/AuthenticationController.java
@@ -0,0 +1,31 @@
+package org.exemple.controller;
+
+import org.exemple.dao.request.SignUpRequest;
+import org.exemple.dao.request.SigninRequest;
+import org.exemple.dao.response.JwtAuthenticationResponse;
+import org.exemple.service.AuthenticationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        return ResponseEntity.ok(authenticationService.signup(request));
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
+        return ResponseEntity.ok(authenticationService.signin(request));
+    }
+}
